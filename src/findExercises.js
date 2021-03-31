ea745fe4f53060e3aa3c4bb4cf05ba8683c579b8
bvinson23 <brandonvinson23@gmail.com>
2021-03-31 11:10:36 -0500

    passed first find test

--- src/findExercises.js
+++ src/findExercises.js
@@ -7,7 +7,9 @@ const instructors = useInstructors();
 // It should accept one integer parameter named `id`
 // It should return the student object with the matching ID
 // Ex: getStudentById(1)
-
+export const getStudentById = (id) => {
+    return students.find(student => student.id === id)
+}
 // Export a function called getInstructorById
 // It should accept one integer parameter named `id`
 // It should return the instructor object with the matching ID
